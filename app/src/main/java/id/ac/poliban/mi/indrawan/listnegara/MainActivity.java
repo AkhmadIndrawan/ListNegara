570cbc7cd2f6a504c86bd42c469422ea5903fb32
AkhmadIndrawan <aindrawan18@gmail.com>
2020-01-02 12:49:15 +0800

    Initial commit

---
+++ app/src/main/java/id/ac/poliban/mi/indrawan/listnegara/MainActivity.java
@@ -0,0 +1,45 @@
+package id.ac.poliban.mi.indrawan.listnegara;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private List<Country> countries = new ArrayList<>();
+    private ListView lvCountry;
+    private BaseAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //koneksikan object dengan view
+        lvCountry = findViewById(R.id.listview);
+
+        //ambil data dari CountryData dan berikan ke object countries
+        countries.addAll(CountryData.getAllCountries());
+
+        //buat object adapter dan kirim data countries sebagai
+        //parameter di konstruktor CountryAdapter
+        adapter = new CountryAdapter(countries);
+
+        //pasang object adapter sebagai adapter lvCountry
+        lvCountry.setAdapter(adapter);
+
+        //menambahkan kejadian ketika user mengklik salah satu item
+        lvCountry.setOnItemClickListener(((parent, view, position, id) ->{
+            new AlertDialog.Builder(this)
+                    .setTitle(countries.get(position).getCountryName())
+                    .setMessage(countries.get(position)
+                            .getCountryDesc())
+                    .setPositiveButton("Ok", null).show();
+        }));
+    }
+}
